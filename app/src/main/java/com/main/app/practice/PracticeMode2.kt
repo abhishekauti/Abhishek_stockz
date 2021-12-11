55945b4c87a329eaf792b8d3bbd183c1d0f675e2
Suraj Verma <surajverma0347@gmail.com>
2021-12-11 18:30:31 +0530

    practice mode two screens added

---
+++ app/src/main/java/com/main/app/practice/PracticeMode2.kt
@@ -0,0 +1,12 @@
+package com.main.app.practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.main.app.R
+
+class PracticeMode2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practice_mode2)
+    }
+}
